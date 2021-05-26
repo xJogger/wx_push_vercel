0960fe905735a597f357a42029016618c6117705
xJogger <xJogger@users.noreply.github.com>
2021-05-26 13:16:25 +0800

    Update wx_push_vercel.py

--- vercel/api/wx_push_vercel.py
+++ vercel/api/wx_push_vercel.py
@@ -62,5 +62,5 @@ def pusher(push_sckey_user):
     else:
         return Response('sckey not right!', mimetype="text/html")
 
-if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+# if __name__ == '__main__':
+#     app.run(debug=True,host="0.0.0.0")
