4ac862d6a9512227319711d55a248a2b6fb82278
xJogger <xJogger@users.noreply.github.com>
2021-05-26 12:48:13 +0800

    init

---
+++ vercel/api/wx_push_vercel.py
@@ -0,0 +1,66 @@
+import os
+import json
+import requests
+from flask import Flask, Response, redirect,url_for,request,abort
+
+def get_tocken(corp_id,agent_secret):
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corp_id}&corpsecret={agent_secret}'
+    response = requests.get(url)
+    return response.json()['access_token']
+
+def push_msg(agent_id,access_token,msg_content):
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+               "touser" : "@all",
+               "toparty" : "",
+               "totag" : "",
+               "msgtype" : "text",
+               "agentid" : agent_id,
+               "text" : {
+                   "content" : msg_content
+               },
+               "safe":0,
+               "enable_id_trans": 0,
+               "enable_duplicate_check": 0,
+               "duplicate_check_interval": 1800
+            }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()
+
+
+app = Flask(__name__)
+@app.route('/<push_sckey_user>.send', methods=['GET', 'POST'])
+def pusher(push_sckey_user):
+    corp_id      = os.getenv('CORP_ID')       # 企业ID
+    agent_id     = int(os.getenv('AGENT_ID')) # 应用ID(int型)
+    agent_secret = os.getenv('AGENT_SECRET')  # 应用的Secrst
+    push_sckey   = os.getenv('PUSH_SCKEY')   # 推送程序的的SCKEY
+
+    if push_sckey_user == push_sckey :
+        if request.method == 'GET':
+            msg_content = request.args.get('text')
+        elif request.method == 'POST':
+            msg_content = request.form['text']
+        else:
+            Response('method not right!', mimetype="text/html")
+
+        try:
+            access_token = get_tocken(corp_id,agent_secret)
+        except Exception as e:
+            Response('con not access_token!', mimetype="text/html")
+        
+        try:
+            response     = push_msg(agent_id,access_token,msg_content)
+        except Exception as e:
+            Response('con not push_msg!', mimetype="text/html")
+
+        if response['errmsg'] == 'ok':
+            Response('push msg succeed!', mimetype="text/html")
+        else:
+            Response('push msg failed!', mimetype="text/html")
+    else:
+        return Response('sckey not right!', mimetype="text/html")
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0")
